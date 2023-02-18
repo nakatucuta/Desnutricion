0a502fbcd1a2e29a3eecb141d496b9c634aba166
juan suarez <juancamilosuarezcantero@gmail.com>
2023-02-17 22:34:19 -0500

    modficiacion de la parte de ingresos no vota error el datatble aunque este vacio

--- resources/views/ingreso/pdf.blade.php
+++ resources/views/ingreso/pdf.blade.php
@@ -181,7 +181,44 @@
 
     
                                     Hola <strong> {{ auth()->user()->name }}</strong> la <strong>  E.P.S.I ANAS WAYUU</strong> <br> <br>
-                                    Ha Recepcionado la informacion de manera exitosa el dia: <strong>{{Carbon\Carbon::now()->format('d-m-Y')}}</strong>
+                                     Ha Recepcionado la informacion de manera exitosa los datos ingresados son los siguientes: {{--el dia: <strong>{{Carbon\Carbon::now()->format('d-m-Y')}}</strong><br> --}}
+
+    @php
+    $user_id = Auth::id(); // Obtener el ID del usuario activo
+    $today = date('Y-m-d'); // Obtener la fecha de hoy
+    $ingresos = DB::table('ingresos as i')
+    ->select('pri_nom_','i.id','seg_nom_','pri_ape_','seg_ape_',
+    'i.created_at','num_ide_')
+    ->join('sivigilas as m', 'i.sivigilas_id', '=', 'm.id' )
+    ->where('i.user_id', $user_id)
+    ->whereDate('i.created_at', $today)->get(); // Obtener los registros de ingresos del usuario activo de hoy
+@endphp
+
+
+@if($ingresos->count() > 0)
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Identificacion</th>
+                <th>Fecha de guardado</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($ingresos as $ingreso)
+                <tr>
+                    <td>{{ $ingreso->id }}</td>
+                    <td>{{ $ingreso->pri_nom_.' '.$ingreso->seg_nom_.' '.$ingreso->pri_ape_ . ' '.$ingreso->seg_ape_}}</td>
+                    <td>{{$ingreso->num_ide_}}</td>
+                    <td>{{ $ingreso->created_at }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@else
+    <p>No se encontraron registros de ingresos para hoy.</p>
+@endif
                                     </p> 
                             </tr>
                                 
