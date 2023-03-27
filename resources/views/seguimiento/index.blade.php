c704f3c4579bfddcb037ec1cc2651abf6e968623
juan suarez <juancamilosuarezcantero@gmail.com>
2023-03-27 16:51:51 -0500

    Mensaje de dddd

--- resources/views/seguimiento/index.blade.php
+++ resources/views/seguimiento/index.blade.php
@@ -239,7 +239,7 @@ right: 0;"><i class="fas fa-book"></i>  </a>
                     <th >Nombre</th>
                     <th >Estado</th>
                     <th >Ips</th>
-                    <th >Fecha proximo controlssss</th>
+                    <th >Fecha proximo controlya</th>
                     <th >Acciones</th>
                   </tr>
                 </thead>
