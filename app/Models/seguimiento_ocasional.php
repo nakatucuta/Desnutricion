c9a49d589d951837ebcf53c752a1d33904d220f7
nakatucuta <juancamilosuarezcantero@gmail.com>
2023-11-17 14:08:29 -0500

    CORRECION EN SEGUIMIENTO COASIONAL

--- app/Models/seguimiento_ocasional.php
+++ app/Models/seguimiento_ocasional.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class seguimiento_ocasional extends Model
 {
     use HasFactory;
+
+    public function getDateFormat(){
+        return 'Y-d-m h:m:s';
+        }
 }
