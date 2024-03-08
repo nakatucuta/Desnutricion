86aef96589052f259831e7164b9d18a78967ef7f
nakatucuta <juancamilosuarezcantero@gmail.com>
2024-03-08 08:03:01 -0500

    nuevo reporte de niños sin seguimientos

--- app/Exports/reportesinseguimientos.php
+++ app/Exports/reportesinseguimientos.php
@@ -5,9 +5,13 @@ namespace App\Exports;
 use App\Models\Sivigila;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Illuminate\Support\Facades\DB;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
 
-class reportesinseguimientos implements FromCollection
+class reportesinseguimientos implements FromCollection, WithHeadings, ShouldAutoSize, WithEvents
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -25,5 +29,71 @@ class reportesinseguimientos implements FromCollection
     
 
         return $resultados;
+    }
+
+
+    public function headings(): array
+    {
+        return [
+            'Ips asignada',
+            'id',
+            'Cod eve',
+            'Semana de notificacion',
+            'Ultima  fecha de notificacion',
+            'Año',
+            'Departamento',
+            'Municipio',
+            'Tipo de identificacion',
+            'Identificacion',
+            'Primer nombre',
+            'Segundo nombre',
+            'Primer apellido',
+            'Segundo apellido',
+            'Edad',
+            'Sexo',
+            'Fecha de nacimiento',
+            'Edad en meses',
+            'Telefono',
+            'Etnia',
+            'Regimen',
+            'Ips atencion Ambulatorio',
+            'Estado',
+            'Fecha atencion Inicial',
+           
+           
+            'Caso confirmada de desnutricion (etiologia-primaria)',
+            'Ips Manejo Hospitalario',
+            'nombre hospitalario',
+            
+            
+            'Id prestador',
+            'fecha creacion',
+            'fecha de edicion'
+
+         
+        ];
+    
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class    => function(AfterSheet $event) {
+                $cellRange = 'A1:AD1'; // All headers
+                $event->sheet->getDelegate()->getStyle($cellRange)->getFont()->setSize(14);
+                $event->sheet->getDelegate()->getStyle($cellRange)->getFont()->setBold(true);
+                $event->sheet->getDelegate()->getStyle($cellRange)->getFill()
+                ->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
+                ->getStartColor()->setARGB('e6ffe6');
+                $event->sheet->getDelegate()->getStyle($cellRange)->getAlignment()->setHorizontal('center');
+                $event->sheet->setAutoFilter($cellRange);
+                $event->sheet->getDelegate()->getStyle('B2:V200')->applyFromArray([
+                    'alignment' => [
+                        'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT,
+                    ]
+                ]);
+            },
+        ];
+
     }
 }
