38b1754b331655b07a2330e9aea092a3e9d6d49e
nakatucuta <juancamilosuarezcantero@gmail.com>
2025-01-08 10:49:37 -0500

    TEMAS DE ACTUALIZACION A LARAVEL 11

--- config/app.php
+++ config/app.php
@@ -195,7 +195,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
-        Yajra\DataTables\DataTablesServiceProvider::class,
 
 
     ],
@@ -213,7 +212,6 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
-         'DataTables' => Yajra\DataTables\Facades\DataTables::class,
   
 
     ])->toArray(),
