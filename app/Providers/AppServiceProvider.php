5936482ccb7e23b7b1f0ac75bd791e9695eccd1a
nakatucuta <juancamilosuarezcantero@gmail.com>
2025-04-28 10:38:15 -0500

    a ver si funciona el iframe

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator; 
+use Illuminate\Support\Facades\View;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
