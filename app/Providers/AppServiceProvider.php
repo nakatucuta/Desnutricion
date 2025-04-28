69352a0799a6c89d99eed642a7d0a76e8513bebf
nakatucuta <juancamilosuarezcantero@gmail.com>
2025-04-28 11:20:56 -0500

    a ver si funciona el iframe

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator; 
-use Illuminate\Support\Facades\View;
-
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -26,6 +24,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
-        View::share('iFrameEnabled', true);
     }
 }
--- resources/views/sivigila/index.blade.php
+++ resources/views/sivigila/index.blade.php
@@ -1,4 +1,4 @@
-@extends('adminlte::page', ['iFrameEnabled' => true])
+@extends('adminlte::page')
 
 @section('title', 'Anas wayuu')
 
