96d8b7159a282d1376850afa7a73279df8f5c103
nakatucuta <juancamilosuarezcantero@gmail.com>
2025-12-18 15:53:57 -0500

    modificacion a error  del endpoint de solo nuevos

--- app/Models/ApiConsumptionState.php
+++ app/Models/ApiConsumptionState.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class ApiConsumptionState extends Model
 {
+     public function getDateFormat(){
+        return 'Y-d-m h:m:s';
+        }
     protected $table = 'api_consumption_states';
 
     protected $fillable = [
