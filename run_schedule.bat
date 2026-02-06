d2f5b16b5b55e7c04f2a46bafe6e68c3f271a3b7
nakatucuta <juancamilosuarezcantero@gmail.com>
2026-02-05 22:46:46 -0500

    modificacion del schedule de gestantes

--- run_schedule.bat
+++ run_schedule.bat
@@ -4,9 +4,18 @@ setlocal
 set "PROJECT=C:\Apache24\htdocs\Desnutricion"
 set "LOG=%PROJECT%\storage\logs\alertas.log"
 set "PHP=C:\php\php.exe"
+set "LOCK=%PROJECT%\storage\logs\seguimientos_enviar_alertas.lock"
 
+if exist "%LOCK%" (
+  echo === %date% %time% - SKIP (lock exists) ===>> "%LOG%"
+  exit /b 0
+)
+
+echo LOCK>%LOCK%
 echo === %date% %time% - seguimientos:enviar-alertas ===>> "%LOG%"
+
 cd /d "%PROJECT%"
 "%PHP%" artisan seguimientos:enviar-alertas >> "%LOG%" 2>&1
 
+del "%LOCK%" >nul 2>&1
 endlocal
