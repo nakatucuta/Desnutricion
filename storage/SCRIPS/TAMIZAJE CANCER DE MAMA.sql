6a5872f52ae1260e779ceaa0988c4b4d56e80a81
nakatucuta <juancamilosuarezcantero@gmail.com>
2026-03-28 15:37:34 -0500

    cursos de vida

--- storage/SCRIPS/TAMIZAJE CANCER DE MAMA.sql
+++ storage/SCRIPS/TAMIZAJE CANCER DE MAMA.sql
@@ -8,10 +8,10 @@ ON A.tipoIdentificacion=C.tipoidentificacion AND A.identificacion=C.identificaci
 
 inner join sga..maestroAfiliados d
 on c.numeroCarnet=d.numeroCarnet
-WHERE A.codigoProcedimiento IN ('876802') 
-
-and a.diagnostico in ('Z000')AND
-a.finalidadProcedimiento IN ('4','7') 
+WHERE A.codigoProcedimiento IN ('876802')
+AND ISNULL(d.genero, '') = 'F'
+AND a.diagnostico in ('Z000','Z123')
+AND a.finalidadProcedimiento IN ('1')
 AND a.fechaProcedimiento between '2022-01-01' and '2025-06-30'
 
 
